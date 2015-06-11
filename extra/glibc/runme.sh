bc0fdf8215a8e17f667d3c4c1e3351a4b166d3d2
wuyazi <zhangy@dtdream.com>
2015-06-11 21:59:20 +0800

    setup ok

---
+++ extra/glibc/runme.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#set -x
+
+basepath=$(cd `dirname $0`; pwd)
+
+buildpath=$(dirname $basepath)/glibc_build
+echo $basepath $buidlpath
+
+if [ "${basepath}" != "${buildpath}" ] ; then
+	rm -rf ${buildpath}
+	mkdir -p ${buildpath}
+	cp -p $0 ${buildpath}/
+	pushd ${buildpath}
+	. ./$0 $*
+	popd
+	rm -rf ${buildpath}
+	exit $?
+fi
+
+export CFLAGS="-g -O2"
+../glibc/configure  --enable-add-ons  --disable-sanity-checks  --libdir=/lib
+
+
+CPUS=`grep processor /proc/cpuinfo| wc -l`
+
+make -j ${CPUS} 
+
+#install needed libs
+
+DEST_LIB_PATH=${basepath}/../../target/x86-64/fs/lib
+[ ! -d ${DEST_LIB_PATH} ] && mkdir -p ${DEST_LIB_PATH}
+[ ! -L ${DEST_LIB_PATH}64 ] && ln -s ./lib ${DEST_LIB_PATH}64
+
+cp -f ./elf/ld-linux-x86-64.so.2 	${DEST_LIB_PATH}/
+cp -f ./libc.so.6 			${DEST_LIB_PATH}/
+cp -f ./math/libm.so.6 			${DEST_LIB_PATH}/
+cp -f ./nptl/libpthread.so.0 		${DEST_LIB_PATH}/
+cp -f ./login/libutil.so.1 		${DEST_LIB_PATH}/
+
+
+
+#./elf/ld-linux-x86-64.so.2
+#./libc.so.6
+#./resolv/libanl.so.1
+#./resolv/libresolv.so.2
+#./resolv/libnss_dns.so.2
+#./nss/libnss_files.so.2
+#./nss/libnss_db.so.2
+#./rt/librt.so.1
+#./libidn/libcidn.so.1
+#./nptl_db/libthread_db.so.1
+#./hesiod/libnss_hesiod.so.2
+#./nis/libnsl.so.1
+#./nis/libnss_nis.so.2
+#./nis/libnss_nisplus.so.2
+#./nis/libnss_compat.so.2
+#./math/libm.so.6
+#./dlfcn/libdl.so.2
+#./locale/libBrokenLocale.so.1
+#./login/libutil.so.1
+#./crypt/libcrypt.so.1
+#./nptl/libpthread.so.0
+
--- runme.sh
+++ runme.sh
@@ -1,4 +1,44 @@
 #!/bin/bash
+#set -x
+
+EXTRA=0
+KERNEL=0
+
+compile_kernel(){
+	pushd os
+	sh ./runme.sh
+	popd
+}
+
+compile_extra(){
+	pwd
+	for i in $(ls extra)
+	do
+		echo build $i
+		pushd ./extra/$i
+		sh ./runme.sh
+		popd
+	done
+}
 
 
+while test $# -gt 0; do
+case "$1" in
+	--with-extra)
+		EXTRA=1
+		shift
+	;;
+	--with-kernel)
+		KERNEL=1
+		shift
+	;;
 
+	*)
+		echo Unkown paramenter!
+		exit 1
+	;;
+esac
+done
+
+[ "${EXTRA}" == "1" ] && compile_extra
+[ "${KERNEL}" == "1" ] && compile_kernel
